b928c9053fd3f2d98ffd4f3eb3ec7c161192ab43
Cinthia Valero <cvalero@anditi.com>
2021-05-30 17:17:07 +1000

    lesso one

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,8 @@
 module.exports.handler = (evt, ctx, done) => {
-  
-}
+  console.log(evt);
+  if (evt.message !== "hello") {
+    done(new Error("erroooorrrrrr"));
+  } else {
+    done(null, "oki!!!");
+  }
+};
